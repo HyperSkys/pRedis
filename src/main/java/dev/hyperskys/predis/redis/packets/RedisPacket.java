8880c9e2d0301d7cd316fca80a72402cc9bfb963
hyperskys <thebestdude112@gmail.com>
2022-08-18 11:18:58 -0500

    redis packets

--- src/main/java/dev/hyperskys/predis/redis/packets/RedisPacket.java
+++ src/main/java/dev/hyperskys/predis/redis/packets/RedisPacket.java
@@ -6,9 +6,5 @@ import org.json.JSONObject;
  * The base class for all Redis packets.
  */
 public abstract class RedisPacket {
-    /**
-     * The method that is called when a packet is received.
-     * @param jsonObject The JSONObject that was received.
-     */
     public abstract void onReceive(JSONObject jsonObject);
 }
