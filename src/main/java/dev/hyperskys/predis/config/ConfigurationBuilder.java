2629f36909952dd68f95bbd1abf052a5f9ee566f
hyperskys <thebestdude112@gmail.com>
2022-08-19 12:49:58 -0500

    wewe

--- src/main/java/dev/hyperskys/predis/config/ConfigurationBuilder.java
+++ src/main/java/dev/hyperskys/predis/config/ConfigurationBuilder.java
@@ -6,14 +6,12 @@ import lombok.Getter;
 public class ConfigurationBuilder {
 
     private final @Getter RedisDB redisDB;
-    private @Getter Class<?> clazz;
-    private @Getter boolean preventDuplicateInstances;
+    private final @Getter Class<?> clazz;
+    private final @Getter boolean preventDuplicateInstances;
 
     public ConfigurationBuilder(RedisDB redisDB, Class<?> clazz, boolean preventDuplicateInstances) {
         this.redisDB = redisDB;
         this.clazz = clazz;
         this.preventDuplicateInstances = preventDuplicateInstances;
     }
-
-
 }
